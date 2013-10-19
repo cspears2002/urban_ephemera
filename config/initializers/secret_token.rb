75a67d8209c2d20690d86688445708595f239d25
cspears2002 <cspears2002@yahoo.com>
2013-10-18 17:26:35 -0700

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UrbanEphemera::Application.config.secret_key_base = '835d879363cf6b4226b56387a9f2be8454020393b6307bcfa7bd290b764971257bae4c0f06178955ff59108bab31b9177de881e9c825dabb7d6b7a5d4f76f886'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UrbanEphemera::Application.config.session_store :cookie_store, key: '_urban_ephemera_session'
