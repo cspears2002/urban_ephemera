4e8f99a625b8d6641ce3e10ebc380ee5132a00d2
cspears2002 <cspears2002@yahoo.com>
2013-10-21 15:35:34 -0700

    Added a name field to the store model.

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,6 +1,7 @@
 class Store
   include Mongoid::Document
 
+  field :name, type: String
   field :speciality, type: String
   field :avg_rating, type: Float
   field :number_reviews, type: Integer
@@ -14,5 +15,5 @@ class Store
 
   # has_many :reviews, autosave: true
   has_many :reviews
-  
+
 end
