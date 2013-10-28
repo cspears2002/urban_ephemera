90a4564a421398afa101f9a8be6d241358d799cf
cspears2002 <cspears2002@yahoo.com>
2013-10-28 15:17:05 -0700

    Stores now count the number of variables they have received.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -21,6 +21,11 @@ class ReviewsController < ApplicationController
 
     # Set the number of reviews a user has
     current_user.update(number_reviews: current_user.reviews.count)
+
+    # Update fields in store
+    store = Store.find(params[:store_id])
+    store.update(number_reviews: store.reviews.count)
+    
    	redirect_to :action => "show", :id => @review._id
   end
 
