fb77fc69d44dc70a8be4c49ce50e2cbb050b0714
cspears2002 <cspears2002@yahoo.com>
2013-10-29 21:49:40 -0700

    Trying to implement an update for the reviews.  For some reason a new review is being generated instead of an old review being modified.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -17,24 +17,44 @@ class ReviewsController < ApplicationController
     @review = Review.new(params[:review].permit(:rating, :body))
     @review.user = current_user
     @review.store = Store.find(params[:store_id])
-    @review.save
+    if @review.save
 
-    # Set the number of reviews a user has
-    current_user.update(number_reviews: current_user.reviews.count)
+      # Set the number of reviews a user has
+      current_user.update(number_reviews: current_user.reviews.count)
 
-    # Update number_reviews field in store
+      # Update number_reviews field in store
+      store = Store.find(params[:store_id])
+      store.update(number_reviews: store.reviews.count)
+
+      # Get the average rating for a store
+      review_array = store.reviews.to_a
+      rating_sum = 0
+      review_array.each do |review| 
+        rating_sum = rating_sum + review.rating
+      end
+      store.update(avg_rating: rating_sum/store.reviews.count)
+
+   	  redirect_to :action => "show", :id => @review._id
+    else
+
+      redirect_to :action => "update", :id => @review._id
+    end
+  end
+
+  def update
+    # Grab user's review
+    @review = Review.find_by(user: current_user, store: Store.find(params[:store_id]))
+    @review.update(rating: params[:rating].to_f, body: params[:body])
+      
+    # Update the average rating for a store
     store = Store.find(params[:store_id])
-    store.update(number_reviews: store.reviews.count)
-
-    # Get the average rating for a store
     review_array = store.reviews.to_a
     rating_sum = 0
-    review_array.each do |review| 
+    review_array.each do |review|
       rating_sum = rating_sum + review.rating
     end
     store.update(avg_rating: rating_sum/store.reviews.count)
-
-   	redirect_to :action => "show", :id => @review._id
+    redirect_to :action => "show", :id => @review._id
   end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -6,5 +6,7 @@ class Review
 
   belongs_to :user
   belongs_to :store
+
+  validates :user, uniqueness: true
   
 end
